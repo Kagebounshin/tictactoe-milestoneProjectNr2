42e3e91551a58cd425d25cba84ef4274ea8682fa
Kagebounshin <kristoffer-heiskanen@hotmail.com>
2021-04-09 10:06:18 +0000

    added a TO DO list

---
+++ assets/js/gameScript.js
@@ -0,0 +1,16 @@
+console.log('hello');
+
+//TODO 
+// Make the gamboard 
+// Two players, 'X' and 'O'
+// Making your mark on the gameboard, and if you done your mark, you cant change or mark over it. 
+// Take turns, X, O ,X etc...
+// Win by having three in a row, horizontally, vertically or diagonally. 
+// If No winner, it's a tie.
+// The Game is finished, make it possible to reset the gameboard.
+// Make the two player enter their names.
+// 
+
+
+
+// I want to get the 
