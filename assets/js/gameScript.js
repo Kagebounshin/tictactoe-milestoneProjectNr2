1b792cc07b94b9c1ddc88adc7c4fd82ec1ebd900
Kagebounshin <kristoffer-heiskanen@hotmail.com>
2021-04-13 11:24:11 +0000

    added a click event to restart, only click once on xo-containers

--- assets/js/gameScript.js
+++ assets/js/gameScript.js
@@ -2,7 +2,6 @@ console.log('hello');
 
 //TODO 
 // Make the gamboard 
-// Two players, 'X' and 'O'
 // Making your mark on the gameboard, and if you done your mark, you cant change or mark over it. 
 // Take turns, X, O ,X etc...
 // Win by having three in a row, horizontally, vertically or diagonally. 
@@ -15,18 +14,26 @@ var player1 = 'X';
 var player2 = 'O';
 //get the gameboard containers, make them into an array. 
 var xo = Array.from(document.querySelectorAll('.xo-container'));
-console.log(xo);
-//add click to the gameboard, 
-var clicked = document.getElementById('gameboard').addEventListener('click', turnHandler);
+document.getElementById('restart').addEventListener('click', game);
 
-function turnHandler(event) {
-    $('.xo-container').css('background-color', 'red');
+
+// //add click to the xo-containers 
+xo.forEach(container => {
+    container.addEventListener('click', clickHandler, {
+        once: true
+    }); // you can only click on one element once per game. 
+})
+
+// https://dev.to/cilly_boloe/addeventlistener-once-js-bits-565d 
+// for only adding the eventlistener once. 
+
+function clickHandler(event) {
+    console.log('clicked')
 }
 
 // The gameboard, 9 empty strings, to fill with X's & O's. 
 function game() {
 
-    var gameboard = ['X', '', '', '', 'X', 'O', '', '', 'X'];
+    var gameboard = ['X', '', 'X', '', '', '', '', '', ''];
+    console.log('You clicked restart')
 }
-
-game()
