0c90c6540706c30fac371b5eb08f1773e890aee9
Kagebounshin <kristoffer-heiskanen@hotmail.com>
2021-04-19 08:13:04 +0000

    working on countdown timer

--- assets/js/gameScript.js
+++ assets/js/gameScript.js
@@ -1,17 +1,10 @@
-console.log('hello');
-
 //TODO 
-// Make the gamboard 
 // Make so you can input your name as player1 or player2
 // Making your mark on the gameboard, and if you done your mark, you cant change or mark over it. 
 // Taketur ns, X, O ,X etc...
 // Win by having three in a row, horizontally, vertically or diagonally. 
 // If No winner, it's a tie.
 // The Game is finished, make it possible to reset the gameboard.
-
-
-
-
 
 // The players. 
 var player1 = 'X';
@@ -20,10 +13,15 @@ var player2 = 'O';
 var xo = Array.from(document.querySelectorAll('.xo-container'));
 document.getElementById('restart').addEventListener('click', game);
 
+// Countdown Timer
+var counter = document.getElementById('counter');
+var seconds = 20;
+counter.innerHtml = seconds;
 
-// //add click to the xo-containers 
+// add click to the xo-containers 
 // https://dev.to/cilly_boloe/addeventlistener-once-js-bits-565d 
 // for only adding the eventlistener once. 
+
 xo.forEach(container => {
     container.addEventListener('click', clickHandler, {
         once: true
@@ -31,14 +29,16 @@ xo.forEach(container => {
 })
 
 
-
 function clickHandler(event) {
     console.log('clicked')
 }
 
-// The gameboard, 9 empty strings, to fill with X's & O's. 
+
+
+
+// The gameboard, 9 empty strings, to fill with X's & O's.
 function game() {
 
-    var gameboard = ['X', '', 'X', '', '', '', '', '', ''];
+    var gameboard = ['', '', '', '', '', '', '', '', ''];
     console.log('You clicked restart')
 }
