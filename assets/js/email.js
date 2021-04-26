22db99771fcea9fe8cf192bd39dc4aa220889ccc
Kagebounshin <kristoffer-heiskanen@hotmail.com>
2021-04-26 10:39:10 +0000

    Added acknowledgement comments, removed the error console.log

--- assets/js/email.js
+++ assets/js/email.js
@@ -1,3 +1,5 @@
+//This EmailJS script is from on of codeinstitute.net lessons with some modifications. Thanks!
+
 function sendEmail(feedbackForm) {
     emailjs.send("service_ze2p865", "tictactoe-form", {
             "from_name": feedbackForm.name.value,
@@ -8,13 +10,14 @@ function sendEmail(feedbackForm) {
             function (response) {
                 let thanks = document.getElementById("thanks-for-feedback");
                 thanks.innerText = "Thank you for the feedback!";
+                // https://stackoverflow.com/questions/15343890/clear-input-fields-on-form-submit/15344007
+                // For reseting the form after submitting.
                 var form = document.getElementById("form");
                 form.reset();
             },
             function (error) {
                 let errorMessage = document.getElementById("error");
                 errorMessage.innerText = "Something went wrong, try again!";
-                console.log("ERROR");
             }
         );
     return false;
