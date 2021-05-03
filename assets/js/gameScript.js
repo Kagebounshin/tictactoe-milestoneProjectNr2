6d1474807b9e51489cab8f83664190f35b7f276d
Kagebounshin <kristoffer-heiskanen@hotmail.com>
2021-05-03 10:56:23 +0000

    Structured the script a little better, checked if the turnOrder worked properly

--- assets/js/gameScript.js
+++ assets/js/gameScript.js
@@ -1,12 +1,12 @@
 //TODO 
 // Win by having three in a row, horizontally, vertically or diagonally. 
 // If No winner, it's a tie.
-// Start Timer on first click. 
+// Start Timer on first click.  once: true should work. 
 // The Game is finished, make it possible to reset the gameboard.
 
-// Winning Types
+// Winning Options
 
-var winOptions = [
+var winningTypes = [
     [0, 1, 2],
     [0, 4, 8],
     [0, 3, 6],
@@ -18,47 +18,51 @@ var winOptions = [
 ];
 
 
-// The players. 
 var player1 = 'X';
 var player2 = 'O';
-var turnOrder;
+var turnOrder = player1;
 var seconds = 20;
-//get the gameboard containers, make them into an array. 
 var turnMessage = document.getElementById('turn-display')
-var xo = Array.from(document.querySelectorAll('.xo-container'));
+var win;
+var counter;
+//get the gameboard containers, make them into an array. 
+var xoContainer = Array.from(document.querySelectorAll('.xo-container'));
 document.getElementById('restart').addEventListener('click', game);
-// document.getElementById('turn').innerHTML = "It's " + isTurn + "'s turn!";
-// Countdown Timer
 
 
 
-// https://dev.to/cilly_boloe/addeventlistener-once-js-bits-565d 
-// for only adding the eventlistener once. 
-
-// add 'click' to the all the xo-containers.  
-xo.forEach(container => {
-    container.addEventListener('click', clickHandler, {
-        once: true
-    }); // you can only click on one element once per game. 
-})
+function game() {
+    gameboard = [
+        '', '', '',
+        '', '', '',
+        '', '', ''
+    ];
+    // https://dev.to/cilly_boloe/addeventlistener-once-js-bits-565d 
+    // for only adding the eventlistener once. 
+    // add 'click' to the all the xo-containers.  
+    xoContainer.forEach(container => {
+        container.addEventListener('click', clickHandler, {
+            once: true
+        });
+    })
+    countDown()
+}
 
 
 // countdown timer
 function countDown() {
-    setInterval(function () {
+    counter = setInterval(function () {
         if (seconds <= 0) {
             clearCountDown()
-            // $('.gameover').addClass("show");
-            console.log("GAME OVER");
+            // $('.gameover-message').addClass("show");
         }
         document.getElementById('counter').innerHTML = seconds;
         seconds -= 1;
     }, 1000);
 }
 
-
 function clearCountDown() {
-    clearInterval(seconds === 0);
+    clearInterval(counter);
 }
 
 
@@ -67,7 +71,8 @@ function clearCountDown() {
 
 function clickHandler(event) {
 
-    let index;
+    let index = event.target;
+
     for (i = 0; i < index; i++) {
         return index;
     }
@@ -76,25 +81,11 @@ function clickHandler(event) {
 
     if (turnOrder === player1) {
         turnOrder = player2
-        $(this).text(player2) // sets the players mark!
-        turnMessage.innerHTML = player1;
+        console.log(turnOrder)
     } else {
         turnOrder = player1
-        $(this).text(player1)
-        turnMessage.innerHTML = player2;
+        console.log(turnOrder)
     };
 };
-
-
-// The gameboard, 9 empty strings, to fill with X's & O's.
-function game() {
-    gameboard = [
-        '', '', '',
-        '', '', '',
-        '', '', ''
-    ];
-
-    countDown()
-}
 
 game()
