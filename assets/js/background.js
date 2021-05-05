60a707c4a6058976217b73a34ffa296ece57a065
Kagebounshin <kristoffer-heiskanen@hotmail.com>
2021-05-05 11:23:09 +0000

    Made a function that randomizes the bg-color when you click a button in index.html

--- assets/js/background.js
+++ assets/js/background.js
@@ -1,1 +1,9 @@
-console.log('hello')
+var bgButton = document.getElementById('bgButton');
+bgButton.addEventListener("click", bgChange);
+var bgColors = ['#585353', '#7bc36e', 'red', '#7fc3ccc4', '#963385'];
+
+function bgChange() {
+
+    var random = Math.floor(Math.random() * bgColors.length);
+    document.body.style.background = bgColors[random];
+}
