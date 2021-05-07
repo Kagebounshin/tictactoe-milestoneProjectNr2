8c52665024709b5f79ced81111f8bfbab8b3340c
Kagebounshin <kristoffer-heiskanen@hotmail.com>
2021-05-07 07:26:02 +0000

    added a new background color

--- assets/js/background.js
+++ assets/js/background.js
@@ -1,6 +1,6 @@
 var bgButton = document.getElementById('bgButton');
 bgButton.addEventListener("click", bgChange);
-var bgColors = ['#7fc3ccc4', '#847fccc4', '#cc7f7fc4', '#7fcc81c4', '#c7cc7fc4', '#a9a9a9b3', 'ff0000'];
+var bgColors = ['#7fc3ccc4', '#847fccc4', '#cc7f7fc4', '#7fcc81c4', '#c7cc7fc4', '#a9a9a9b3', 'ff0000', 'red'];
 
 var colors = ['#1099ab', '#2c2961', '#b55353', '#1a691c', '#4c5014c4', '#484848', '#710808', 'black'];
 
