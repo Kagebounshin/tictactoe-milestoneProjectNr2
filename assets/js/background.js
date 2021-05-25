ebac281e46ce89d5774be224c0adb64b5167d624
Kagebounshin <kristoffer-heiskanen@hotmail.com>
2021-05-25 16:29:54 +0000

    Removed one of the colors from the color array

--- assets/js/background.js
+++ assets/js/background.js
@@ -1,6 +1,6 @@
 var bgButton = document.getElementById('bgButton');
 bgButton.addEventListener("click", bgChange);
-var bgColors = ['#7fc3ccc4', '#847fccc4', '#cc7f7fc4', '#7fcc81c4', '#c7cc7fc4', '#a9a9a9b3', 'ff0000', 'red'];
+var bgColors = ['#7fc3ccc4', '#847fccc4', '#cc7f7fc4', '#7fcc81c4', '#c7cc7fc4', '#a9a9a9b3', 'ff0000'];
 
 function bgChange() {
     var random = Math.floor(Math.random() * bgColors.length);
